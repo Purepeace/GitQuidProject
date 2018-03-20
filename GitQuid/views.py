b2e80d98ab5163824eddca34ce14796d0c624dec
Berta <2262178k@student.gla.ac.uk>
2018-03-20 17:52:08 +0000

    Login page

--- GitQuid/views.py
+++ GitQuid/views.py
@@ -5,7 +5,6 @@
 # !!!
 
 from django.shortcuts import render
-# from django.http import HttpResponse
 # from GitQuid.models import Project
 # from GitQuid.models import Category
 # from GitQuid.forms import CategoryForm
@@ -13,9 +12,9 @@ from django.shortcuts import render
 
 from GitQuid.forms import UserForm, UserProfileForm
 
-# from django.contrib.auth import authenticate,login
-# from django.http import HttpResponseRedirect, HttpResponse
-# from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 # from django.contrib.auth.decorators import login_required
 # from django.contrib.auth import logout
 # from datetime import datetime
@@ -193,7 +192,7 @@ def register(request):
 
             # Did the user provide a profile picture?
             # If so, we need to get it from the input form and
-            #put it in the UserProfile model.
+            # put it in the UserProfile model.
             if 'picture' in request.FILES:
                 profile.picture = request.FILES['picture']
 
@@ -221,53 +220,44 @@ def register(request):
                    'registered': registered})
 #
 #
-# def user_login(request):
-#     # If the request is a HTTP POST, try to pull out the relevant information.
-#     if request.method == 'POST':
-#
-#         # Gather the username and password provided by the user.
-#         # This information is obtained from the login form.
-#         # We use request.POST.get('<variable>') as opposed
-#         # to request.POST ['<variable>'] because the
-#         # request.POST.get('<varibale>') returns None if the
-#         # value does not exist, while request.POST['<variable>']
-#         # will raise a KeyError exception.
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-#
-#         # Use Django's machinery to attempt to see if the username/password
-#         # combination is valid - a User object is returned if it is.
-#         user = authenticate(username=username, password=password)
-#
-#         # If we have a User object, the details are correct.
-#         # If None(Python's wat of representing the absence of a value), no user
-#         # with matching credentials was found.
-#         if user:
-#             # Is the account active? It could have been disabled.
-#             if user.is_active:
-#                 # If the account is valid and active, we can log the user in.
-#                 # We'll send the user back to the homeproject.
-#                 login(request, user)
-#                 return HttpResponseRedirect(reverse('index'))
-#             else:
-#                 # An inactive account was used - no logging in!
-#                 return HttpResponse("Your GitQuid account is disabled.")
-#         else:
-#             # Bad login details were provided. So we can't log the user in.
-#             print("Invalid login details : {0},{1}".format(username, password))
-#             return HttpResponse("Invalid login details supplied.")
-#
-#         # The request is not a HTTP POST so display the login form.
-#         # This scenario would most likely be a HTTP GET.
-#     else:
-#         # No context variables to pass to the template system, hence the
-#         # blank dictionary object...
-#         return render(request, 'GitQuid/login.html', {})
-#
-#
-# @login_required
-# def retricted(request):
-#     return render(request, 'GitQuid/restricted.html', {})
+def user_login(request):
+    # If the request is a HTTP POST, try to pull out the relevant information.
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+        # We use request.POST.get('<variable>') as opposed
+        # to request.POST['<variable>'], because the
+        # request.POST.get('<variable>') returns None if the
+        # value does not exist, while request.POST['<variable>'] # will raise a KeyError exception.
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Use Django's machinery to attempt to see if the username/password
+        #  combination is valid - a User object is returned if it is.
+        user = authenticate(username=username, password=password)
+
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value), no user # with matching credentials was found.
+        if user:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                #  We'll send the user back to the homepage.
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                # An inactive account was used - no logging in!
+                return HttpResponse("Your account is disabled.")
+
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+
+    # The request is not a HTTP POST, so display the login form. # This scenario would most likely be a HTTP GET.
+    else:
+        # No context variables to pass to the template system, hence the # blank dictionary object...
+        return render(request, 'GitQuid/login.html', {})
 #
 #
 # # Use the login_required() decorator to ensure only those logged in can
