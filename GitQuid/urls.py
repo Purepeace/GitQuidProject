aac46214d92280cfc0c1c1cc687d7c4c8d7281d7
Lakshit Dabas <ldabas@purdue.edu>
2018-03-20 17:50:18 +0000

    Style Dump

--- GitQuid/urls.py
+++ GitQuid/urls.py
@@ -9,7 +9,7 @@ from GitQuid import views
 
 app_name = 'GitQuid'
 urlpatterns = [
-    # url(r'^$',views.index,name='index'),
+    url(r'^$',views.index,name='index'),
     # url(r'^about/',views.about, name='about'),
     # url(r'^add_category/$', views.add_category, name='add_category'),
     # url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
