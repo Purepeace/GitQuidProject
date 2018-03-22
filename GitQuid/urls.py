c40e4551084b7c0ed90033d9ac4b0300efc76f1c
Berta <2262178k@student.gla.ac.uk>
2018-03-22 17:37:47 +0000

    account page skeleton

--- GitQuid/urls.py
+++ GitQuid/urls.py
@@ -9,13 +9,13 @@ from GitQuid import views
 
 app_name = 'GitQuid'
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
+    url(r'^$', views.index, name='index'),
     # url(r'^about/',views.about, name='about'),
     # url(r'^add_category/$', views.add_category, name='add_category'),
     # url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
     #                views.show_category, name='show_category'),
     # url(r'^category/(?P<category_name_slug>[\w\-]+)/add_project/', views.add_project, name='add_project'),
-    # url(r'^restricted/',views.restricted,name='restricted'),
+    url(r'^account/$', views.account, name='account'),
     url(r'^logout/$', views.user_logout, name='logout'),
     url(r'^register/$', views.register, name='register'),
     url(r'^projects/$', views.browse_projects, name='browse_projects'),
