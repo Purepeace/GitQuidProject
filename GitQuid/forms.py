79796ede59ca8de9513be716f993b4c55a898a61
Berta <2262178k@student.gla.ac.uk>
2018-03-26 01:58:54 +0100

    A very simple edit page, problems with saving certain info, accessible through GitQuid/account/edit for now

--- GitQuid/forms.py
+++ GitQuid/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import *
 from GitQuid.models import UserProfile, Category, Project
 from markdownx.fields import MarkdownxFormField
+from django.contrib.auth.forms import UserChangeForm
 
 
 class CategoryForm(forms.ModelForm):
@@ -86,3 +87,34 @@ class ProjectForm(forms.ModelForm):
             raise forms.ValidationError(
                 "No such category exists, you cheeky hacker"
             )
+
+
+class EditProfileForm(UserChangeForm):
+
+    class Meta:
+
+        model = User
+
+        fields = {
+            'email',
+            'password',
+
+        }
+
+
+class EditRestForm(UserChangeForm):
+
+    class Meta:
+
+        model = UserProfile
+
+        fields = {
+
+            'picture',
+            'description',
+            'password',
+
+        }
+
+
+
